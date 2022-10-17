6532665e2735fe69edbb88f0ad9812ede6456b20
Refazul <refazul.refat@yahoo.com>
2022-10-17 18:40:04 +0600

    modernize-1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::get('login', 'Auth\LoginController@showLoginForm');
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::get('myphpinfo', 'HomeController@phpfileinfo');
 
